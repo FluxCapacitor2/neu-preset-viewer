500aa767c43f407eb208c1dff49a6374a6690df2
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-11-23 00:43:41 -0500

    Add title and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,13 @@
   import PresetSubmitForm from "../components/PresetSubmitForm.svelte";
 </script>
 
+<svelte:head>
+  <title>NEU Preset Viewer</title>
+  <meta
+    name="description"
+    content="View NotEnoughUpdates configuration presets for Hypixel Skyblock, including custom enchant color and custom todo preset types." />
+</svelte:head>
+
 <a href="/">
   <h1 class="my-8 text-center text-3xl font-bold">NEU Preset Viewer</h1>
 </a>
